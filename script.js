6d5e0b1de396cfcd1280f988a9a99096274b3e1a
Subhrojit Mondal <subhromond@gmail.com>
2021-06-17 22:24:56 +0530

    fUN

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 let game = document.querySelector(".game");
-game.addEventListener("click",()=>{
-    let result = document.querySelector(".result");
-    result.style.fontSize = 300+"px";
-    result.style.textAlign = "center";
-    result.textContent = Math.round(Math.random()*10)%6;
-})
+let result = document.querySelector(".result");
+let arr = ["Movies", "Food", "Buy-anything", "Socialize", "Games"];
+let movies = ["harrypotter", "marvel", "Rocky", "animated-films"];
+
+game.addEventListener("click", () => {
+  if (result.childNodes) {
+    let rem = result.childNodes;
+    for (let j = 0; j < rem.length; j++) {
+      rem[j].remove();
+    }
+  }
+  result.style.fontSize = 300 + "px";
+  let res = Math.round(Math.random() * 10) % 6;
+  if (res == 0) {
+    res = res + 1;
+  }
+  for (let i = 1; i <= arr.length; i++) {
+    if (i == res) {
+      let child = document.createElement("button");
+      child.classList.add(arr[i - 1]);
+      child.classList.add("add");
+      child.textContent = arr[i - 1] + " " + i ;
+      result.appendChild(child);
+    }
+  }
+});
+
+/*
+1-movie ["harrypotter","marvel","Rocky","animated-films"]
+2-food
+3-buy anything
+4-talk to anyone
+5-Games
+*/
