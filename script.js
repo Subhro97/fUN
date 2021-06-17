9497bc4ccbdbed5c674737f59e24ee7e8f78efb7
Subhrojit Mondal <subhromond@gmail.com>
2021-06-17 21:31:47 +0530

    fUN

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ game.addEventListener("click",()=>{
     let result = document.querySelector(".result");
     result.style.fontSize = 300+"px";
     result.style.textAlign = "center";
-    result.textContent = Math.round(Math.random()*10);
+    result.textContent = Math.round(Math.random()*10)%6;
 })
